820c4fbc6326a5a4af6b1cf22dcbd5ea5e0387f1
Netsanet-dev <Netsanetsolomon41@gmail.com>
2025-02-02 00:12:13 +0300

    Adding task 2 file

--- task_2.sql
+++ task_2.sql
@@ -1,34 +1,32 @@
-CREATE DATABASE IF NOT EXISTS alx_book_store;
-USE alx_book_store;
-CREATE TABLE authors(
+CREATE TABLE Authors(
 author_id INT PRIMARY KEY,
 author_name VARCHAR(215)
 );
-CREATE TABLE books(
+CREATE TABLE Books(
 book_id INT PRIMARY KEY,
 title VARCHAR(130),
 author_id INT,
-FOREIGN KEY (author_id) REFERENCES authors(author_id),
+FOREIGN KEY (author_id) REFERENCES Authors(author_id),
 price DOUBLE,
 publication_date DATE
 );
-CREATE TABLE customers(
+CREATE TABLE Customers(
 customer_id INT PRIMARY KEY,
 customer_name VARCHAR(215),
 email VARCHAR(215),
 address TEXT
 );
-CREATE TABLE orders(
+CREATE TABLE Orders(
 order_id INT PRIMARY KEY,
 customer_id INT,
-FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
+FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
 order_date DATE
 );
-CREATE TABLE order_details(
+CREATE TABLE Order_Details(
 order_detail_id INT PRIMARY KEY,
 order_id INT,
 book_id INT,
-FOREIGN KEY (order_id) REFERENCES orders(order_id),
-FOREIGN KEY (book_id) REFERENCES books(book_id),
+FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+FOREIGN KEY (book_id) REFERENCES Books(book_id),
 quantity DOUBLE
 );
