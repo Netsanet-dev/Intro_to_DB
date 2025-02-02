d9a4e222e8582699ac0926cfb99d2f43b17b2ce3
Netsanet-dev <Netsanetsolomon41@gmail.com>
2025-02-02 11:06:00 +0300

    Adding mysql server file

--- MySQLServer.py
+++ MySQLServer.py
@@ -10,7 +10,7 @@ mydb = mysql.connector.connect(
 mycursor = mydb.cursor()
 try:
     mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
-except:
+except mysql.connector.Error:
     print("Database Not created.")
 else:
     print(f"Database 'alx_book_store' created successfully!")
